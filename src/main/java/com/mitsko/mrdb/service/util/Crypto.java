578c50bb00d9670374272642c55cf5b26133749c
DavidMitsko <45606856+DavidMitsko@users.noreply.github.com>
2020-02-02 20:55:53 +0300

    Changed md5 to bcrypt

--- src/main/java/com/mitsko/mrdb/service/util/Crypto.java
+++ src/main/java/com/mitsko/mrdb/service/util/Crypto.java
@@ -1,10 +1,20 @@
 package com.mitsko.mrdb.service.util;
 
-import org.apache.commons.codec.digest.DigestUtils;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 public class Crypto {
+    private final String gensalt;
 
-    public String encode(String str) {
-        return DigestUtils.md5Hex(str);
+    public Crypto() {
+        gensalt = BCrypt.gensalt();
+    }
+
+    public String encode(String password) {
+        return BCrypt.hashpw(password, gensalt);
+    }
+
+    public boolean checkPassword(String password, String hashPassword) {
+        return BCrypt.checkpw(password, hashPassword);
     }
 }
